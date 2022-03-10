2aec4eef8ad2517b8834abad14bae009566163e2
durgap09 <90268800+durgap09@users.noreply.github.com>
2022-03-10 07:39:04 +0530

    Create setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+export JDBC_DRIVER_2=org.postgresql.Driver
+export JDBC_URL_2=jdbc:postgresql://172.31.24.246:5432/exec_module
+export POSTGRES_USERNAME_2=exec_module
+export POSTGRES_PASSWORD_2=exec_module1010#
+export WORKING_DIR_2=/home/execution-module/
+export PULUMI_BASE_URL=http://172.31.24.246:3000/
+export SES_ACCESS_KEY=AKIAQCCE4QSRF6ZQOTA4
+export SES_SECRET_KEY=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
+export SES_REGION=ap-south-1
+export SES_MAIL_SENDER=support@appmodernize.com
+export CHANGE_PASSWORD_LINK=https://qa.test.appmodz.net/register/
+export FORGOT_PASSWORD_LINK=https://qa.test.appmodz.net/forgot_password/
+export LOGIN_LINK=https://qa.test.appmodz.net/login/
